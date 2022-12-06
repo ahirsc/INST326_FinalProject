7ab12afb53f86793336471a567da17462156e2bb
Aline Hirsch <alinehirsch@outlook.com>
2022-12-05 19:35:02 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,59 +1,49 @@
 import turtle
 
-def main():
-
+class Drawings():
+  
   jim = turtle.Turtle() #Remember the capital T
   jim.speed(5) #A speed of 0 makes him go as fast as possible
 
   arm_length = 100 #Change these if you want
   leg_length = 120
 
-  def reset(): #reset sends him back to the center
+  def reset(self,jim): #reset sends him back to the center
     jim.pu() #pu is short for penup. Either work, you can use them interchangeably
     jim.setpos(0,0)
     jim.pd() #pd is short for pendown
 
-
-  #draw a stickman
-
-  reset()
-
   #head
-  jim.seth(90) #seth is short for set_heading, and it changes the direction jim is facing.
-  jim.fd(30)
-  jim.rt(90)
-  jim.circle(50)
-
-  reset()
+  def head(self,jim):
+    jim.seth(90) #seth is short for set_heading, and it changes the direction jim is facing.
+    jim.fd(30)
+    jim.rt(90)
+    jim.circle(50)
 
   #arm 1
-  jim.seth(160)
-  jim.fd(arm_length/2)
-  jim.rt(20)
-  jim.fd(arm_length/2)
+  def arm_1(self,jim):
+    jim.seth(160)
+    jim.fd(Drawings.arm_length/2)
+    jim.rt(20)
+    jim.fd(Drawings.arm_length/2)
 
-  reset()
 
   #arm 2
-  jim.seth(20)
-  jim.fd(arm_length/2)
-  jim.lt(20)
-  jim.fd(arm_length/2)
+  def arm_2(self,jim):
+    jim.seth(20)
+    jim.fd(Drawings.arm_length/2)
+    jim.lt(20)
+    jim.fd(Drawings.arm_length/2)
 
-  reset()
 
   #leg 1
-  jim.seth(270)
-  jim.fd(150)
-  jim.seth(230)
- 
-
-  reset()
+  def leg_1(self,jim):
+    jim.seth(270)
+    jim.fd(150)
+    jim.seth(230)
 
   #leg 2
-  jim.seth(270)
-  jim.fd(150)
-  jim.seth(310)
-
-
-main()
+  def leg_2(self,jim):
+    jim.seth(270)
+    jim.fd(150)
+    jim.seth(310)
