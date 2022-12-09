09cdcee2767275f0974e39e660505ccb457e8ae6
Aline Hirsch <alinehirsch@outlook.com>
2022-12-08 21:11:13 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,133 +1,179 @@
+import random
 import turtle
-import random 
-"""
-class Drawings():
+print("Let's play hangman!\n")
+
+#Turtle
+t = turtle.Turtle()
+t.ht()
+t.lt(180)
+t.pensize(2)
+t.penup()
+t.rt(-90)
+t.fd(150)
+t.rt(90)
+t.bk(-50)
+t.pendown()
+t.pencolor("black")
+    
+def draw_post():
+  #drawing of the base of the post
+  t.bk(100)
+  t.fd(200)
+
+  #drawing of the post
+  t.bk(100)
+  t.rt(90)
+  t.fd(280) #height of the post
+  t.lt(-90)
+  t.fd(140)
+  t.lt(-90)
+  t.fd(50)
   
-  jim = turtle.Turtle() #Remember the capital T
-  jim.speed(5) #A speed of 0 makes him go as fast as possible
-
-  arm_length = 100 #Change these if you want
-  leg_length = 120
-
-  def reset(self,jim): #reset sends him back to the center
-    jim.pu() #pu is short for penup. Either work, you can use them interchangeably
-    jim.setpos(0,0)
-    jim.pd() #pd is short for pendown
-
+def draw_head():
   #head
-  def head(self,jim):
-    jim.seth(90) #seth is short for set_heading, and it changes the direction jim is facing.
-    jim.fd(30)
-    jim.rt(90)
-    jim.circle(50)
+  t.dot(50)
 
-  #arm 1
-  def arm_1(self,jim):
-    jim.seth(160)
-    jim.fd(Drawings.arm_length/2)
-    jim.rt(20)
-    jim.fd(Drawings.arm_length/2)
+def draw_body():
+  #body
+  t.fd(130)
+  t.lt(-180)
+  t.fd(80)
 
-  #arm 2
-  def arm_2(self,jim):
-    jim.seth(20)
-    jim.fd(Drawings.arm_length/2)
-    jim.lt(20)
-    jim.fd(Drawings.arm_length/2)
+def draw_leftArm():
+  #Left arm
+  t.rt(-100)
+  t.fd(65)
+  t.lt(180)
+  t.fd(65)
 
-  #leg 1
-  def leg_1(self,jim):
-    jim.seth(270)
-    jim.fd(150)
-    jim.seth(230)
+def draw_rightArm():
+  #right arm
+  t.rt(20)
+  t.fd(65)
+  t.lt(-180)
+  t.fd(65)
+  #move
+  t.penup()
+  t.rt(-100)
+  t.pendown()
 
-  #leg 2
-  def leg_2(self,jim):
-    jim.seth(270)
-    jim.fd(150)
-    jim.seth(310)
-"""
-class word_works(object):
+def draw_leftLeg():  
+  #leg1
+  t.fd(85)
+  t.lt(-35)
+  t.fd(65)
+  t.lt(-180)
+  t.fd(65)
 
-  def generate_word():
-    words = {
-      1 : "community",
-      2 : "volcano",
-      3 : "sunrise",
-      4 : "hypothesis",
-      5 : "information",
-      6 : "sample",
-      7 : "probability",
-      8 : "Brazil",
-      9 : "railroad",
-      10 : "diamond",
-      11 : "question",
-      12 : "agreement",
-      13 : "reflection",
-      14 : "elephant",
-      15 : "pigeon",
-      16 : "technology",
-      17 : "multiply",
-      18 : "appetite",
-      19 : "designer",
-      20 : "function",
-      21 : "pandemic",
-      22 : "interface",
-      23 : "helicopter",
-      24 : "algorithm",
-      25 : "orange"
-    }
-    word_key = random.randint(1, 25)
-    selected_word = words[word_key]
-    return selected_word
+def draw_rightLeg():  
+  #leg2
+  t.lt(-110)
+  t.fd(65)
 
-  def display_word(self,selected_word):
-    word = selected_word.generate_word()
-    length_of_word = len(word)
-    display_letterinword = "_"*(length_of_word)
-    print(display_letterinword)
+def generate_word():
+  words = {
+    1: "community",
+    2: "volcano",
+    3: "sunrise",
+    4: "hypothesis",
+    5: "information",
+    6: "sample",
+    7: "probability",
+    8: "Brazil",
+    9: "railroad",
+    10: "diamond",
+    11: "question",
+    12: "answer",
+    13: "reflection",
+    14: "elephant",
+    15: "pigeon",
+    16: "technology",
+    17: "multiply",
+    18: "appetite",
+    19: "designer",
+    20: "function",
+    21: "pandemic",
+    22: "interface",
+    23: "helicopter",
+    24: "algorithm",
+    25: "orange"
+  }
+  word_key = random.randint(1, 25)
+  selected_word = words[word_key]
+  return selected_word
 
 
+word = generate_word()
+length_of_word = len(word)
+display_letterinword = "_" * (length_of_word)
+print(display_letterinword)
 number_of_guesses = 0
 correct_guesses = 0
 incorrect_guesses = 0
 guesses = []
+dummy_list = []
+draw_post()
 
-def repeated_letter(x,y):
+
+def repeated_letter(x, y):
   if x in y:
     print("\nThis letter has already been guessed!")
-    print("List of guessed letters:", guesses,"\n")
+    print("List of guessed letters:", guesses, "\n")
   else:
     pass
 
-def list_duplicates_of(seq,item):
-    start_at = -1
-    locs = []
-    while True:
-        try:
-            loc = seq.index(item,start_at+1)
-        except ValueError:
-            break
-        else:
-            locs.append(loc)
-            start_at = loc
-    return locs
-  
+
+def list_duplicates_of(seq, item):
+  start_at = -1
+  locs = []
+  while True:
+    try:
+      loc = seq.index(item, start_at + 1)
+    except ValueError:
+      break
+    else:
+      locs.append(loc)
+      start_at = loc
+  return locs
+
 
 while number_of_guesses < length_of_word:
-  guess = input("\nEnter a letter to guess: ")
-  
+  try:
+    guess = str(input("\nEnter a letter to guess: "))
+  except:
+    print("Only letters please")
+  if len(guess) != 1:
+    print("Only one letter at a time please")
+
   repeated_letter(guess, guesses)
-  
+  if guess in guesses:
+    dummy_list.append(guess)
+  else:
+    guesses.append(guess)
+
+    
   if guess not in word:
-    incorrect_guesses+=1
-    
+    incorrect_guesses += 1
+    print(incorrect_guesses)
+    if incorrect_guesses == 1:
+      draw_head()
+    elif incorrect_guesses == 2:
+      draw_body()
+    elif incorrect_guesses == 3:
+      draw_leftArm()
+    elif incorrect_guesses == 4:
+      draw_rightArm()
+    elif incorrect_guesses == 5:
+      draw_leftLeg()
+    elif incorrect_guesses == 6:
+      draw_rightLeg()
+
   while guess in word:
-  
-    position = list_duplicates_of(word,guess)
+
+    position = list_duplicates_of(word, guess)
     print(position)
-    counter= 0
-    
+    counter = 0
+
     for i in position:
       #
       index = position[counter]
@@ -136,33 +182,24 @@ while number_of_guesses < length_of_word:
 
       #you can only insert a new item into a certain index if it's a list
       display_letterinword.insert(index, guess)
-      display_letterinword.pop(index+1)
-    
+      display_letterinword.pop(index + 1)
+
       #turn the list back into a string
       display_letterinword = ''.join(display_letterinword)
       correct_guesses += 1
-      
-      counter+=1
-    print (display_letterinword)
-    number_of_guesses +=1
+
+      counter += 1
+    print(display_letterinword)
+    number_of_guesses += 1
     break
     
   if correct_guesses == length_of_word:
     print("You guessed correctly!")
     break
+    
   if incorrect_guesses == 7:
     print("You reached the guessing limit")
+    print("The word was: ", word)
     break
   
-    #problem: same letter in two different positions, how do we make sure both go in? --- solved
 
-  
-
-
-
-
-
-
-
-  
- 
